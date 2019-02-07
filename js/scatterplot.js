2d0c1fe4043c53c005005b730bba74c7dd092d3f
John Reidy <jackbreidy@gmail.com>
2019-02-07 02:31:11 -0800

    scaling working

--- js/scatterplot.js
+++ js/scatterplot.js
@@ -10,23 +10,15 @@ let svg = cardBody.append('svg')
 	.attr('width', '100%')
 	.attr('height', getSvgTargetHeight() + 'px');
 
-// plot data values (TO DO: remove plot var entirely)
-var plot = {
-	'minX': 0,
-	'maxX': 100,
-	'minY': 0,
-	'maxY': 100
-}
-
 function getXScale() {
 	return d3.scaleLinear()
-	    .domain([plot.minX, plot.maxX])
-	    .range([0, getSvgTargetWidth()]);
+	    .domain([minMaxDict[selectColX.property('value')]["min"], minMaxDict[selectColX.property('value')]["max"]])
+	    .range([0, getSvgTargetWidth() - 80]);
 }
 function getYScale() {
 	return d3.scaleLinear()
-	    .domain([plot.minY, plot.maxY])
-	    .range([getSvgTargetHeight(), 0]);
+	    .domain([minMaxDict[selectColY.property('value')]["min"], minMaxDict[selectColY.property('value')]["max"]])
+	    .range([getSvgTargetHeight() - 50, 0]);
 }
 
 let plotGroup = svg.append('g')
@@ -38,9 +30,11 @@ let xAxis = plotGroup.append('g')
     .attr('class', 'axis');
 let yAxis = plotGroup.append('g')
     .attr('id', 'y-axis')
-    .attr('class', 'axis');
+    .attr('class', 'axis')
+    .attr('transform', `translate(${0},${-1*getSvgMarginOffset() + 90})`);
 let pointsGroup = plotGroup.append('g')
-	.attr('id', 'points-group');
+	.attr('id', 'points-group')
+	.attr('transform', `translate(${0},${-1*getSvgMarginOffset() + 90})`);
 
 let dataBlob;
 
@@ -49,18 +43,35 @@ let selectColY = d3.select('#select-col-y').on('change', updateColumnY);;
 
 window.addEventListener("resize", resize);
 
+let minMaxDict = [];
+// parse csv to dictionary and create min max dictionary
 function parseData(d) {
 	return {
-		id: +d.id,
-		loanAmnt: +d.loan_amnt,
-		intRate: +d.int_rate,
-		dti: +d.dti,
-		totalACC: +d.total_acc
+		id: updateMinMaxDict('id',+d.id),
+		loan_amnt: updateMinMaxDict('loan_amnt',+d.loan_amnt),
+		int_rate: updateMinMaxDict('int_rate',+d.int_rate),
+		dti: updateMinMaxDict("dti",+d.dti),
+		total_acc: updateMinMaxDict('total_acc',+d.total_acc)
 	}
 }
+// check key value pair for min and max
+// this is necessary for plot axis scaling
+function updateMinMaxDict(key, value) {
+	if (key in minMaxDict) {
+		if (minMaxDict[key]["min"] > value) minMaxDict[key]["min"] = value;
+		if (minMaxDict[key]["max"] < value) minMaxDict[key]["max"] = value;
+	} else {
+		minMaxDict[key] = [];
+		minMaxDict[key]["min"] = value;
+		minMaxDict[key]["max"] = value;
+	}
+	return value;
+}
+
 function loadData(error, d) {
 	if (error) throw error;
 	dataBlob = d;
+	console.log(minMaxDict);
 	initializeOptions();
     drawData();
 }
@@ -99,25 +110,21 @@ function drawData() {
 	drawPoints();
 }
 function drawPoints() {
-
 	let updatedPoints = pointsGroup.selectAll('circle.point-thing').data(dataBlob);
-
-	console.log(selectColX.property('value') + ' ' + selectColY.property('value'));
-
-
-let xScaleObj = getXScale();
+	// convenience and speed
+	let xScaleObj = getXScale();
 	let yScaleObj = getYScale();
+	// create
 	updatedPoints.enter()
 		.append('circle')
 		.attr('class', 'point-thing')
         .attr('r', 3.0)
         .style('fill', 'rgba(14, 100, 210, 0.7)')
         .attr('cx', function (d) { return xScaleObj(d[selectColX.property('value')]); })
-                 .attr('cy', function (d) { return yScaleObj(d[selectColY.property('value')]); });;
-
+		.attr('cy', function (d) { return yScaleObj(d[selectColY.property('value')]); });
+	// delete
     updatedPoints.exit().remove();
-
-    
+    // updated existing elements
     updatedPoints.attr('cx', function (d) { return xScaleObj(d[selectColX.property('value')]); })
                  .attr('cy', function (d) { return yScaleObj(d[selectColY.property('value')]); });
 }
@@ -125,14 +132,6 @@ let xScaleObj = getXScale();
 // events (TO DO maybe just bind events to drawData ) 
 function resize() {
 	drawData(dataBlob); 
-}
-// update X with new column
-function updateX() {
-	drawData(dataBlob);
-}
-// update Y with new column
-function updateY() {
-	drawData(dataBlob);
 }
 // update x column option
 function updateColumnX() {
