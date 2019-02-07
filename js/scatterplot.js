26e394e1ecd559a33c3ba875fb189d9f142c90c2
John Reidy <jackbreidy@gmail.com>
2019-02-06 17:59:54 -0800

    working on resizing, removing background for now

--- js/scatterplot.js
+++ js/scatterplot.js
@@ -1,10 +1,10 @@
 // plot data values
 var plot = {
-	'minX': 0,
-	'maxX': 10000000,
+	'minX': 6,
+	'maxX': 10,
 	'minY': 0,
-	'maxY': 10000000,
-	'width': 300,
+	'maxY': 10000,
+	'width': 600,
 	'height': 300,
 	'margin': 100
 }
@@ -20,32 +20,40 @@ let yScale = d3.scaleLinear()
 // create svg
 let cardBody = d3.select('#svg-container');
 let svg = cardBody.append('svg')
+	.attr('id', 'svg')
 	.attr('width', '100%')
 	.attr('height', '500px');
-cardBody.onresize = function(){ console.log("hi") }; // not working..
-
-// svg.append("rect")
-// 	.attr("width", "100%")
-// 	.attr("height", "100%")
-// 	.attr("fill", "rgb(72, 84, 101)");
 
 let plotGroup = svg.append('g')
 	.attr('id', 'plot-group');
+
+let xAxis = plotGroup.append('g')
+    .attr('transform', `translate(0,${plot.height})`)
+    .attr('id', 'x-axis')
+    .attr('class', 'axis')
+    .call(d3.axisBottom(xScale));
+let yAxis = plotGroup.append('g')
+    .attr('id', 'y-axis')
+    .attr('class', 'axis')
+    .call(d3.axisLeft(yScale));
 let pointsGroup = plotGroup.append('g')
 	.attr('id', 'points-group');
 
-let pointsData;
 let plotBackground;
+
+// ^ variables initialized, elements added to DOM
+
+window.addEventListener("resize", resize);
 
 function parseData(d) {
 	return {
 		id: d.id,
-		loanAmnt: +d.loan_amnt
+		loanAmnt: +d.loan_amnt,
+		intRate: +d.int_rate
 	}
 }
 function loadData(error, d) {
 	if (error) throw error;
-	pointsData = d;
 	plotBackground = plotGroup.append('rect')
         .attr('id', 'plot-background')
         .attr('x', 0)
@@ -53,8 +61,9 @@ function loadData(error, d) {
         .attr('width', plot.width)
         .attr('height', plot.height)
         .attr('fill', '#fff')
-        .attr('border', '1px')
         .on('mousedown', plotBackgroundMouseDown);
+
+    plotBackground.lower();
 
     drawData(d);
 }
@@ -63,32 +72,37 @@ d3.csv("data/test.csv", parseData, loadData);
 function drawData(d) {
 	drawScatterPlot(d);
 }
-function drawScatterPlot(data) {
-	let pointData = data;
+function drawScatterPlot(d) {
+
 	let points = pointsGroup.selectAll('circle.pp');
-	let updatedPoints = points.data(pointData, d => d.id);
+	let updatedPoints = points.data(d, d => d.id);
 
 	updatedPoints.enter()
 		.append('circle')
 		.attr('class', 'pp')
         .attr('r', 2.5)
-        .attr('cx', function (d) { return xScale(d.id); })
+        .attr('cx', function (d) { return xScale(d.intRate); })
         .attr('cy', function (d) { return yScale(d.loanAmnt); })
-        .style('fill', '#9de0a3')
-        .style('stroke', '#17471b')
-        .style('stroke-opacity', 1.0)
-        .style('stroke-width', 0.3);
+        .style('fill', 'rgba(14, 100, 210, 0.7)');
 
     updatedPoints.exit().remove();
 
 }
 
 // events
-function plotBackgroundMouseDown() {
-	console.log("plot background mouse down");
+function resize() {
+	svg.node().getBoundingClientRect()
+	console.log(svg.node().getBoundingClientRect());
+	redraw();
 }
-
-function svgResize() {
-	console.log("resize me");
-	// need to update relevant plot struct values
+// update X with new column
+function updateX(d) {
+	redraw(d);
+}
+// update Y with new column
+function updateY(d) {
+	redraw(d);
+}
+function redraw(d) {
+	drawData(d);
 }
