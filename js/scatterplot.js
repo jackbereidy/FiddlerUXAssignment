68f192da20ed620b894696dbd7fea9bb32355e27
John Reidy <jackbreidy@gmail.com>
2019-02-07 17:53:20 -0800

    working on range slider next

--- js/scatterplot.js
+++ js/scatterplot.js
@@ -10,21 +10,30 @@ let svg = cardBody.append('svg')
 	.attr('width', '100%')
 	.attr('height', getSvgTargetHeight() + 'px');
 
+// scale real X value to plot value (with min, max and spacer)
+// if (min - spacer) is small or less than zero, ignore spacer
 function getXScale() {
+	let min = minMaxDict[selectColX.property('value')]["min"];
+	let max = minMaxDict[selectColX.property('value')]["max"];
+	let spacer = (max - min) * 0.1;
 	return d3.scaleLinear()
-	    .domain([minMaxDict[selectColX.property('value')]["min"], minMaxDict[selectColX.property('value')]["max"]])
+	    .domain([(min - spacer > 1) ? min - spacer : 0, max + spacer])
 	    .range([0, getSvgTargetWidth() - 80]);
 }
+// scale real Y value to plot value (with min, max and spacer)
+// if (min - spacer) is small or less than zero, ignore spacer
 function getYScale() {
+	let min = minMaxDict[selectColY.property('value')]["min"];
+	let max = minMaxDict[selectColY.property('value')]["max"];
+	let spacer = (max - min) * 0.1;
 	return d3.scaleLinear()
-	    .domain([minMaxDict[selectColY.property('value')]["min"], minMaxDict[selectColY.property('value')]["max"]])
+	    .domain([(min - spacer > 1) ? min - spacer : 0, max + spacer])
 	    .range([getSvgTargetHeight() - 50, 0]);
 }
 
 let plotGroup = svg.append('g')
 	.attr('id', 'plot-group')
 	.attr('transform', `translate(${getSvgMarginOffset()},${-1*getSvgMarginOffset()})`);
-
 let xAxis = plotGroup.append('g')
     .attr('id', 'x-axis')
     .attr('class', 'axis');
@@ -32,6 +41,15 @@ let yAxis = plotGroup.append('g')
     .attr('id', 'y-axis')
     .attr('class', 'axis')
     .attr('transform', `translate(${0},${-1*getSvgMarginOffset() + 90})`);
+
+let plotLabel = d3.select('#plotLabel');
+
+let xLabelGroup = plotGroup.append('g');
+let xLabel = xLabelGroup.append('text');
+
+let yLabelGroup = plotGroup.append('g');
+let yLabel = yLabelGroup.append('text').attr('transform', 'rotate(-90)');
+
 let pointsGroup = plotGroup.append('g')
 	.attr('id', 'points-group')
 	.attr('transform', `translate(${0},${-1*getSvgMarginOffset() + 90})`);
@@ -44,18 +62,20 @@ let selectColY = d3.select('#select-col-y').on('change', updateColumnY);;
 window.addEventListener("resize", resize);
 
 let minMaxDict = [];
-// parse csv to dictionary and create min max dictionary
+// parse csv to dictionary and update min max.
+// we only store keys with numeric values because 
+// there isn't a intuitive way to plot non-numeric
+//  values in a scatterplot
 function parseData(d) {
-	return {
-		id: updateMinMaxDict('id',+d.id),
-		loan_amnt: updateMinMaxDict('loan_amnt',+d.loan_amnt),
-		int_rate: updateMinMaxDict('int_rate',+d.int_rate),
-		dti: updateMinMaxDict("dti",+d.dti),
-		total_acc: updateMinMaxDict('total_acc',+d.total_acc)
+	let dict = [];
+	for (var key in d) {
+		if (!isNaN(+d[key])) {
+			dict[key] = updateMinMaxDict(key, +d[key]);
+		}
 	}
+	return dict;
 }
-// check key value pair for min and max
-// this is necessary for plot axis scaling
+// check key value pair for min and max - necessary for plot axis scaling
 function updateMinMaxDict(key, value) {
 	if (key in minMaxDict) {
 		if (minMaxDict[key]["min"] > value) minMaxDict[key]["min"] = value;
@@ -67,13 +87,11 @@ function updateMinMaxDict(key, value) {
 	}
 	return value;
 }
-
 function loadData(error, d) {
 	if (error) throw error;
 	dataBlob = d;
-	console.log(minMaxDict);
 	initializeOptions();
-    drawData();
+    redraw();
 }
 d3.csv("data/test.csv", parseData, loadData);
 
@@ -98,7 +116,7 @@ function initializeOptions() {
 		.attr('selected', 'selected');
 }
 
-function drawData() {
+function redraw() {
 	// update svg height (TO DO - do conditionally)
 	svg.attr('height', getSvgTargetHeight() + 'px');
 	// update x axis with new window (TO DO - do conditionally)
@@ -106,6 +124,23 @@ function drawData() {
 		.call(d3.axisBottom(getXScale()));
 	// update y axis with new window (TO DO - do conditionally)
 	yAxis.call(d3.axisLeft(getYScale()));
+	// update plot label
+	plotLabel.text(selectColX.property('value') + ' vs ' + selectColY.property('value'));
+
+
+
+
+	// set text and location of x label
+
+	// xLabelGroup.attr('transform', `translate(${xAxis},${0})`);
+	// xLabel.text(selectColX.property('value'));
+
+	    // getSvgTargetWidth() - 80
+
+	let yLabelYPosition = getSvgTargetHeight()/2 - yLabel.node().getBBox().height/2;
+	yLabelGroup.attr('x', 100)
+	    .attr('y', yLabelYPosition);
+	yLabel.text(selectColY.property('value'));
 
 	drawPoints();
 }
@@ -129,17 +164,18 @@ function drawPoints() {
                  .attr('cy', function (d) { return yScaleObj(d[selectColY.property('value')]); });
 }
 
-// events (TO DO maybe just bind events to drawData ) 
+// events (TO DO maybe just bind events to redraw ) 
 function resize() {
-	drawData(dataBlob); 
+	redraw(dataBlob); 
 }
 // update x column option
 function updateColumnX() {
-	drawData(dataBlob);
+	redraw(dataBlob);
 }
 // update y column option
 function updateColumnY() {
-	drawData(dataBlob);
+	redraw(dataBlob);
 }
 
 };
+
